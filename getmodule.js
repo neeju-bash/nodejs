5c593446cd25c7cf62be0b874c5d37b27cef6174
neeju-bash <neerajmohan@gmail.com>
2022-06-23 12:38:36 +0530

    .

--- getmodule.js
+++ getmodule.js
@@ -1,8 +1,6 @@
-var stuff = require('./moduleexport.js');// | ./ | is used to search in present directory.
+var stuff = require('./moduleexport.js');
 
 
-// look here at the counter part, it is actually taking the arguments. and doing everything.
-// We need to intialise the 'counter variable first.'
 console.log(stuff.counter(['Hello','This is','Saif']));
 console.log(stuff.adder(5,6));
 console.log(stuff.pi);
