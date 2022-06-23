8e13e7103b2c3f8d561651d6b4e3a7f61bfea16e
neeju-bash <neerajmohan@gmail.com>
2022-06-23 15:22:45 +0530

    filop

--- fileop.js
+++ fileop.js
@@ -1,3 +1,10 @@
 var fs=require('fs');
 var readMe=fs.readFileSync('export.txt','utf8');
-console.log(readMe);
+//synchrouno=us operation blocks the execution of code until fully read
+fs.writeFileSync('exportnew.txt',readMe);
+
+fs.readFile('exportnew.txt','utf8',function(err,data){
+//not blocking the code asynchrounus
+    console.log(data);
+});
+console.log('execute code while reading file')
