7361b74bfcbb9f21f519a251ef4cd0e889596eee
neeju-bash <neerajmohan@gmail.com>
2022-06-24 16:18:02 +0530

    added html page

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var http = require('http'); // Import Node.js core module
 
 var server = http.createServer(function (req, res) {   //create web server
@@ -14,15 +15,17 @@ var server = http.createServer(function (req, res) {   //create web server
     else if (req.url == "/student") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
+       // res.write('<html><body><p>This is student Page.</p></body></html>');
+       // res.end();
+        fs.ReadStream('index.html').pipe(res);
     
     }
     else if (req.url == "/admin") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
+        //fstat.createReadstream
+       res.write('<html><body><p>This is admin Page.</p></body></html>');
+       res.end();
     
     }
     else
